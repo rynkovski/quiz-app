0499bde720d09bcd7632e008a27433d92d388784
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-12-18 19:27:59 +0100

    feat: update backend

--- apps/backend/src/quiz/quiz.controller.ts
+++ apps/backend/src/quiz/quiz.controller.ts
@@ -1,34 +1,11 @@
-import { Controller, Get, Param, Post, Body, Request } from '@nestjs/common';
+import { Controller, Post, Body, Request } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { CategoryResponseDto } from './dto/category.dto';
-import { QuestionResponseDto } from './dto/question.dto';
 import { QuizService } from './quiz.service';
 
 @ApiTags('quiz')
 @Controller('quiz')
 export class QuizController {
   constructor(private readonly quizService: QuizService) {}
-  @Get('categories')
-  @ApiOperation({ summary: 'Pobierz wszystkie kategorie' })
-  @ApiResponse({
-    status: 200,
-    description: 'Lista kategorii',
-    type: [CategoryResponseDto],
-  })
-  getCategories() {
-    return this.quizService.getCategories();
-  }
-
-  @Get('questions/:categoryId')
-  @ApiOperation({ summary: 'Pobierz pytania dla danej kategorii' })
-  @ApiResponse({
-    status: 200,
-    description: 'Lista pytań',
-    type: [QuestionResponseDto],
-  })
-  getQuestions(@Param('categoryId') categoryId: string) {
-    return this.quizService.getQuestionsByCategory(categoryId);
-  }
 
   @Post('check-answer')
   @ApiOperation({ summary: 'Sprawdź odpowiedź' })
