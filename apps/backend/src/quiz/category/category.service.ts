bc35ed840cb5634a1835db783dd39a671e51be88
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-12-19 01:42:16 +0100

    feat: add cascading deleting

--- apps/backend/src/quiz/category/category.service.ts
+++ apps/backend/src/quiz/category/category.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
@@ -49,14 +54,46 @@ export class CategoryService {
       throw new NotFoundException(`Category with ID ${id} not found`);
     }
   }
-
   async remove(id: string) {
     try {
+      const category = await this.prisma.category.findUnique({
+        where: { id },
+        include: {
+          _count: {
+            select: { questions: true },
+          },
+        },
+      });
+
+      if (!category) {
+        throw new NotFoundException(`Category with ID ${id} not found`);
+      }
+
+      await this.prisma.quizResult.deleteMany({
+        where: {
+          question: {
+            categoryId: id,
+          },
+        },
+      });
+
+      await this.prisma.question.deleteMany({
+        where: {
+          categoryId: id,
+        },
+      });
+
       return await this.prisma.category.delete({
         where: { id },
       });
     } catch (error) {
-      throw new NotFoundException(`Category with ID ${id} not found`);
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new NotFoundException(`Category with ID ${id} not found`);
+        }
+      }
+      console.error('Error deleting category:', error);
+      throw new InternalServerErrorException('Failed to delete category');
     }
   }
 }
